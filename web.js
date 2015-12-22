598e4ff45bbe6c7b7e931914aede5cb056249d66
Peter W <techieshark@gmail.com>
2015-12-22 22:20:10 +1300

    pair status now persists via MongoDB

--- web.js
+++ web.js
@@ -4,50 +4,16 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var logfmt = require("logfmt");
 var request = require('request');
+var MongoClient = require('mongodb').MongoClient;
+var MongoPairList = require('./lib/mongo_pair_list.js');
 
-
+var db;
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(logfmt.requestLogger());
 
-var users = [];
-
-function setof(status, users) {
- return _.filter(users, { 'status': status }).map(function(user) {
-   return ">*" + user.username + "*: " + user.comment;
- }).join('\n');
-}
-
 var help = 'Usage:\t `/pair [yes|ok|no <your project here>]` or `/pair` alone to see who is free.';
-
-function getCurrentPairStatus(users) {
-  var status = '', yes, no, ok;
-
-  yes = setof('yes', users);
-  ok = setof('ok', users);
-  no = setof('no', users);
-
-
-  if (yes.length > 0) {
-    status = '*Yes! Let\'s pair. Come find me now:*\n';
-    status += yes;
-  }
-  if (ok.length > 0 ) {
-    status += '\n*Ok. I\'m working but you can interrupt:*\n';
-    status += ok;
-  }
-  if (no.length > 0 ) {
-    status += '\n*Nope. Do Not Disturb:*\n';
-    status += no;
-  }
-  if (status === '') {
-    status = 'No one up for pairing (yet!). Pair up, yo.\n';
-  } else {
-    status += '\n Pair up, yo. (Go find \'em!) \n';
-  }
-  return status;
-}
 
 function validToken(token) {
   if (token == process.env.SLACK_TOKEN) {
@@ -81,21 +47,6 @@ function notifyChannel(text) {
   );
 }
 
-function updateUsers (username, status, comment) {
-  var user = _.find(users, {'username': username});
-  if (user) {
-    user.status = status;
-    user.comment = comment;
-  }
-  else {
-    user = {'username': username, 'status': status, 'comment': comment};
-    users.push(user);
-  }
-  console.log(users);
-  return user;
-}
-
-
 
 app.post('/', function(req, res) {
   var hasArgs = req.body.text.length > 0;
@@ -105,6 +56,7 @@ app.post('/', function(req, res) {
       token = req.body.token,
       acceptable = ["yes", "ok", "no"];
   var user, status, notification;
+  var pairList;
 
     console.log('args');
     console.log(args);
@@ -125,26 +77,37 @@ app.post('/', function(req, res) {
           var comment = args.slice(1).join(' ');
           status = args[0];
 
-          user = updateUsers(username, status, comment);
+          // get pairList from DB
+          pairList = new MongoPairList(db, process.env.SLACK_TOKEN);
+          pairList.fetch(function (err, pairList) {
+            pairList.update(username, status, comment, function (err, data) {
+              user = data.user; // users = data.users
 
-          // People want confirmation that we got their status, so show it and everyone else's:
-          notification = 'Your pairing status was set to: ' + status + '\n';
-          notification += getCurrentPairStatus(users);
-          res.send(notification);
+              // People want confirmation that we got their status, so show it and everyone else's:
+              notification = 'Your pairing status was set to: ' + status + '\n';
+              notification += pairList.toString();
+              res.send(notification);
 
-          // notify pairing channel if environment vars are provided and status is yes/ok
-          if (process.env.SLACK_WEBHOOK_URL && process.env.SLACK_PAIR_CHANNEL &&
-              (status === 'yes' || status === 'ok')) {
-            notifyChannel(user.username + " says '" + user.status + "' to pairing" + (user.comment ? " (" + user.comment + ")" : "" ) + "! Go pair!");
-          }
+              // notify pairing channel if environment vars are provided and status is yes/ok
+              if (process.env.SLACK_WEBHOOK_URL && process.env.SLACK_PAIR_CHANNEL &&
+                  (status === 'yes' || status === 'ok')) {
+                notifyChannel(user.username + " says '" + user.status + "' to pairing" + (user.comment ? " (" + user.comment + ")" : "" ) + "! Go pair!");
+              }
+            });
+          });
         } else {
           res.send('Close but no cigar. What is this command, "' + args[0] + '", that you speak of?\n' + help);
         }
       }
       else {
-        status = getCurrentPairStatus(users);
-        console.log(status);
-        res.send(status);
+        // get pairList from DB
+        pairList = new MongoPairList(db, process.env.SLACK_TOKEN);
+        pairList.fetch(function (err, pairList) {
+          if(err) throw err;
+          status = pairList.toString();
+          console.log(status);
+          res.send(status);
+        });
       }
     }
 });
@@ -158,12 +121,27 @@ function keepalive() {
   request(process.env.PAIRBOT_URL + '/keepalive');
 }
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-  console.log("PAIR URL: " + process.env.PAIRBOT_URL);
-  console.log("SLACK_TOKEN: " + process.env.SLACK_TOKEN);
-  console.log("SLACK_WEBHOOK_URL: " + process.env.SLACK_WEBHOOK_URL);
-  console.log("SLACK_PAIR_CHANNEL: " + process.env.SLACK_PAIR_CHANNEL);
-  setInterval(keepalive, 60e3);
+// Initialize connection once
+var dbURL = process.env.MONGO_URL;
+if (!dbURL || dbURL === '') {
+  console.error("Error: Missing MONGO_URL environment variable.");
+  process.exit(1);
+}
+
+MongoClient.connect(dbURL, function(err, database) {
+  if(err) throw err;
+
+  db = database;
+
+  // Start the application after the database connection is ready
+  var port = Number(process.env.PORT || 5000);
+  app.listen(port, function() {
+    console.log("Listening on " + port);
+    console.log("PAIR URL: " + process.env.PAIRBOT_URL);
+    console.log("SLACK_TOKEN: " + process.env.SLACK_TOKEN);
+    console.log("SLACK_WEBHOOK_URL: " + process.env.SLACK_WEBHOOK_URL);
+    console.log("SLACK_PAIR_CHANNEL: " + process.env.SLACK_PAIR_CHANNEL);
+    console.log("MONGO_URL: " + process.env.MONGO_URL);
+    setInterval(keepalive, 60e3);
+  });
 });
